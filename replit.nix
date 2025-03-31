e6ec79d969674d20027b63db83b0843b77ece13d
joshua hon <40967090-firefoxhon333@users.noreply.replit.com>
2025-03-31 06:08:02 +0000

    Updated bot and fixed math questions

--- replit.nix
+++ replit.nix
@@ -1,8 +1,26 @@
 {pkgs}: {
   deps = [
+    pkgs.nano
+    pkgs.glibcLocales
+    pkgs.cacert
+    pkgs.pkg-config
+    pkgs.libffi
+    pkgs.zlib
+    pkgs.tk
+    pkgs.tcl
+    pkgs.openjpeg
+    pkgs.libwebp
+    pkgs.libtiff
+    pkgs.libjpeg
+    pkgs.libimagequant
+    pkgs.lcms2
+    pkgs.freetype
+    pkgs.libxcrypt
+    pkgs.mercurial
     pkgs.unzip
     pkgs.wget
-    pkgs.ollama
+    pkgs.python311Full
+    pkgs.python311Packages.pip
     pkgs.tesseract
   ];
 }
